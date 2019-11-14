bff8dd119dad92f4ca430a40b1fd2891d6ec9426
ericurse <siyuanfish@163.com>
2019-11-14 22:24:41 +0800

    first commit!  1019111114

---
+++ helloc.c
@@ -0,0 +1,11 @@
+#include <stdio.h> /*加载标准输入输出函数原型*/
+
+int main(int argc,char * argv[]) /* 主函数，形参暂时不做解释，照着写就可以了 */
+
+{ //{代表函数开始
+
+    puts("Hello World!"); //puts函数打印一个字符串，字符串为"Hello World!"，记住所有的语句都是以;结束。
+
+    return 0;  //返回给操作系统0
+
+}//}代表函数结束
